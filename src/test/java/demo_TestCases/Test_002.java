5d973648630e5009eef36e5410f384fff7d3960e
unknown <sandytocode@gmail.com>
2019-06-02 08:08:44 +0530

    My 1st Push

---
+++ src/test/java/demo_TestCases/Test_002.java
@@ -0,0 +1,36 @@
+package demo_TestCases;
+
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.chrome.ChromeDriver;
+import org.openqa.selenium.chrome.ChromeOptions;
+import org.testng.annotations.BeforeClass;
+import org.testng.annotations.Test;
+
+public class Test_002 {
+	
+	 WebDriver driver;
+
+		@BeforeClass
+		public void setProerty() throws Throwable
+
+		{
+			System.setProperty("webdriver.chrome.driver", "D:\\Selenium\\chromedriver_win32\\chromedriver.exe");
+			ChromeOptions ops = new ChromeOptions();
+			ops.addArguments("--disable-notifications"); /* To disable the Notifications */
+			driver = new ChromeDriver(ops);
+
+		}
+
+		@Test
+
+		public void verify() throws Throwable
+		{
+			driver.get("https://www.twitter.com");
+			driver.manage().window().maximize();
+			// Actual title
+			String title = driver.getTitle();
+			System.out.println("Title is" + title);
+			driver.close();
+			
+		}
+}
